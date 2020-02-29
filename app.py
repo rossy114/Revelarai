d1c1be811d29b5cdf78c3d42f2caf665a9f4125f
Ross <rf@Rosss-MacBook-Pro.local>
2020-02-29 13:07:55 +0000

    Fixed not importing db when importing running the app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-if __name__ == '__main__':
-	from db import db
+# if __name__ == '__main__':
+# 	from db import db
 
 from flask import Flask
 from flask_restful import Api
@@ -32,10 +32,9 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-# if __name__ == '__main__':
-    #from db import db
-    # from db import db
-    # db.init_app(app)
-    # app.run(port=5000, debug=True)
-db.init_app(app)
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
+# db.init_app(app)
+# app.run(port=5000, debug=True)
