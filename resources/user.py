e971992536c4607126cf9cf2e9524eea157b2374
Ross <rf@Rosss-MacBook-Pro.local>
2020-03-05 15:38:10 +0000

    Including delete user and find user

--- resources/user.py
+++ resources/user.py
@@ -21,7 +21,24 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
-        user = UserModel(data['username'], data['password'])
+        user = UserModel(**data)
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
+
+
+class User(Resource):
+    @classmethod
+    def get(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted'}, 200
